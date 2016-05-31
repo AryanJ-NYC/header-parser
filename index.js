c69a2bc78520f1a974f5738101784341158b0fcb
Aryan Jabbari <aryan.jabbari21@qmail.cuny.edu>
2016-05-30 20:19:13 -0400

    Optimize user-agent parsing

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 "use strict"
 const express = require('express'),
-      app = express();
-
-let portNumber = process.env.PORT || process.argv[2] || 8080;
+      app = express(),
+      useragent = require('useragent'),
+      portNumber = process.env.PORT || process.argv[2] || 8080;
 
 app
 .get('/api/whoami', function (request, response) {
+  let agent = useragent.parse(request.headers['user-agent']);
   let userInformation = {};
   userInformation["ipaddress"] = request.ip;
   userInformation["language"] = request.acceptsLanguages()[0];
-  userInformation["software"] = request.headers["user-agent"];
+  userInformation["software"] = agent.os.toString();
   response.json(userInformation);
+  response.end();
+})
+.get('*', function (request, response) {
+  response.end("Please visit " + request.hostname + "/api/whoami for header information.");
 })
 
 app.listen(portNumber);
